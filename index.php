c651ffd368a0218a456045f6c4577f663c1b1182
JoshuaPrabhakar <101444219+JoshuaPrabhakarP@users.noreply.github.com>
2022-05-12 11:46:11 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EducationalRealm | Online learning platform | Future skills </title>
+    <link rel="stylesheet" href="css/stylms.css">
+    <link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+    <header>
+        <a href="index.php" style="text-decoration: none;">
+            <h1 class='logo'>Educational Realm</h1>
+        </a>
+    </header>
+
+    <main>
+        <div class="hero-content">
+            <p class="p1">STRIVE For PROGRESS</p>
+            <p class="p1" style="margin-top: -16px">Not PERFECTION</p>
+            <p class="p2">The beautiful thing about learning is that no one can take it away from you.</p>
+            <a href="pages/signup.php">
+                <input class='signup-butt' type="button" value="Sign Up"></a>
+            <a href="pages/login.php">
+                <input class='login-butt' type="button" value="Login"></a>
+        </div>
+        <div class="hero-image">
+            <img src="background/hero.png" alt="hero-image">
+        </div>
+    </main>
+
+    <footer>
+        <p><small>Copyright &copy; 2022 Joshuva Prabhakar</small></p>
+    </footer>
+</body>
+
+</html>
